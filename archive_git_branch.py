ac0751f09e047361a37a10520af13d5a9cd756ae
hoangchieng282 <hoangchiengphi@gmail.com>
2022-09-23 14:19:30 +0700

    hehehehe

--- archive_git_branch.py
+++ archive_git_branch.py
@@ -1,39 +1,48 @@
 import argparse
 from git import Repo
 
-
+# Author: chih
+# Time modified:   09/23/2022
 parser = argparse.ArgumentParser()
 
 # group = parser.add_mutually_exclusive_group()
 
-parser.add_argument("-r", "--repo", type=str,
-                   help="the git repository")
+# **************************************** Argument declare *******************************************
+parser.add_argument("-r", "--repopath", type=str, help="the path to your local git repository. Just leave it empty")
 
-parser.add_argument("-b", "--branch", type=str,
-                   help="Branch to be archived")
+parser.add_argument("-b", "--branch", type=str, help="Branch to be archived")
 
-parser.add_argument("-t", "--target", type=str,
-                   help="target branch to store the archived")
+parser.add_argument("-t", "--target", type=str, help="target branch to store the archived")
 
-parser.add_argument("-u", "--username", type=str,
-                   help="Your username for git login")
+parser.add_argument("-u", "--username", type=str, help="Your username for git login")
 
-parser.add_argument("-p", "--password", type=str,
-                   help="Your otp if you're using github or your password for bitbucket")
+parser.add_argument("-p", "--password", type=str, help="Your otp if you're using github or your password for bitbucket")
 
 args = parser.parse_args()
 branch = args.branch
 target = args.target
-repo = args.repo if args.repo else "."
-# the default value here is just my local path to the custom git folder. You have to modified the path in order to use this cript on default
+repopath = args.repopath if args.repopath else "."
 username = args.username
 password = args.password
+# **************************************** // *******************************************
+repo =  Repo(repopath)              # Repo object set to cloned repo
+assert not repo.bare                # Make sure repo isn't empty
+remote = repo.remote()              # Set to a remote object (Defaults to 'origin') can override with name=...
 
-repos =  Repo(repo)
+# try:
+#     for repo_branch in repo.references:         # loop over the remote branches
+#         # for branch in branches:                 
+#             # if target in repo_branch.name:      # does the branch you want to delete exist in the remote git repo? 
+#                 print("deleting remote branch: %s" % repo_branch)# %s" % repo_branch.remote_head)
+#                 if str(repo_branch) == "origin/release/1.0.4":
+#                     remote.push(refspec=(":%s" % repo_branch.remote_head)) # remote_head = the branch you want to delete Example: "origin/my-branch"
+# except:
+#     print("error")
+
 try:
     print(target)
-    aiss = repos.git.checkout(target)
-    print(aiss)
+    # aiss = repo.git.checkout(target)
+    # print(aiss)
 except:
     raise Exception("Your branch doesn't exist")
     # print("Your branch doesn't exist")
--- changefile.py
+++ changefile.py
@@ -1,1 +1,28 @@
-print("hi")
+import os, shutil
+import git
+
+def clone_repo (remote_url, local_path, branch):                               # Clone a remote repo
+    print("Cloning repo %s" % remote_url)
+    if os.path.isdir(local_path) is True:
+        shutil.rmtree(local_path)
+    git.Repo.clone_from(remote_url, local_path, branch=branch)
+
+def delete_remote_branches(remote_url, branches):
+    cur_dir = os.path.dirname(__file__)
+    local_path="%s/%s" % (cur_dir,repo)
+
+    clone_repo(
+        remote_url=remote_url,                  # The clone url                                                           
+        local_path=local_path,                  # Dir path where you want to clone to
+        branch="master"                         # Branch to clone (Note: Cannot be on the same branch you want to delete)
+    )
+    repo = git.Repo(local_path)                 # Repo object set to cloned repo
+    assert not repo.bare                        # Make sure repo isn't empty
+    remote = repo.remote()                      # Set to a remote object (Defaults to 'origin') can override with name=...
+    for repo_branch in repo.references:         # loop over the remote branches
+        for branch in branches:                 
+            if branch in repo_branch.name:      # does the branch you want to delete exist in the remote git repo? 
+                print("deleting remote branch: %s" % repo_branch.remote_head)
+                remote.push(refspec=(":%s" % repo_branch.remote_head)) # remote_head = the branch you want to delete Example: "origin/my-branch"
+
+# Note: Could add some logic to delete your local cloned repo when you're done
